e8a14ad6817f82538bafc46cdc8da29e9cfbfe6b
adolfousier <adolfousier@gmail.com>
2025-01-05 17:30:39 +0000

    implemented google analytics tag and integrated with search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'  // Add this import
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -22,9 +23,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E06QJGZDPM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-E06QJGZDPM');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
-
